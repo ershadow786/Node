74e8b61f2df3ac171013957631bda76d72872255
anmol singh <ershadow786@gmail.com>
2023-01-14 14:42:13 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/user-routes");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cookieParser());
+app.use(express.json());
+app.use("/api", router);
+
+mongoose
+  .connect(
+    `mongodb+srv://monogo:mongo@cluster0.uxiwgf6.mongodb.net/user?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(5000);
+    console.log("Database is connected! Listening to localhost 5000");
+  })
+  .catch((err) => console.log(err));
+
+
+  // mongodb+srv://ntnrjpt:<password>@clustertemp.gtbestn.mongodb.net/?retryWrites=true&w=majority
